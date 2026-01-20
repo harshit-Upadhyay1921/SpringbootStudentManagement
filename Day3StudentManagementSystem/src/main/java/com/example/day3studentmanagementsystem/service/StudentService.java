80870a646c2df0a6039daccf5b6d65e434327cd6
harshit-Upadhyay1921 <harshitupadhyay1921@gmail.com>
2026-01-20 15:57:10 +0530

    added controller and services

--- Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/service/StudentService.java
+++ Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/service/StudentService.java
@@ -1,8 +1,16 @@
 package com.example.day3studentmanagementsystem.service;
 
+import com.example.day3studentmanagementsystem.model.StudentModel;
+import com.example.day3studentmanagementsystem.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-
+    private StudentRepository repository;
+    public StudentService(StudentRepository repository){
+        this.repository = repository;
+    }
+    public StudentModel addStudent(StudentModel student){
+        return repository.save(student);
+    }
 }
