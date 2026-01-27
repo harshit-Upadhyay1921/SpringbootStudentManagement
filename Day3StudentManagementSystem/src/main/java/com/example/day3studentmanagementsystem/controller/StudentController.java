2740eb6d492bf4f267646a0e19cb0eda6106ad53
harshit-Upadhyay1921 <harshitupadhyay1921@gmail.com>
2026-01-27 15:43:26 +0530

    added additional routes for updation and deletion

--- Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
+++ Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
@@ -2,10 +2,9 @@ package com.example.day3studentmanagementsystem.controller;
 
 import com.example.day3studentmanagementsystem.model.StudentModel;
 import com.example.day3studentmanagementsystem.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -16,5 +15,17 @@ public class StudentController {
     @PostMapping("/add-student")
     public StudentModel addStudent(@RequestBody StudentModel student){
         return service.addStudent(student);
+    }
+    @GetMapping("/get-student")
+    public List<StudentModel> getStudent(){
+        return service.getStudent();
+    }
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id,@RequestBody StudentModel student){
+        return service.updateStudent(id,student);
+    }
+    @DeleteMapping("/delete-student/{id}")
+    public void deleteStudent(@PathVariable String id){
+        service.deleteStudent(id);
     }
 }
--- Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/service/StudentService.java
+++ Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/service/StudentService.java
@@ -4,6 +4,8 @@ import com.example.day3studentmanagementsystem.model.StudentModel;
 import com.example.day3studentmanagementsystem.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
     private StudentRepository repository;
@@ -12,5 +14,23 @@ public class StudentService {
     }
     public StudentModel addStudent(StudentModel student){
         return repository.save(student);
+    }
+    //display student
+    public List<StudentModel> getStudent(){
+        return repository.findAll();
+    }
+    //update student
+    public StudentModel updateStudent(String id,StudentModel student){
+        StudentModel existingStudent = repository.findById(id).orElseThrow(()-> new RuntimeException("No student found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setEmail(student.getEmail());
+        existingStudent.setAge(student.getAge());
+
+        return repository.save(existingStudent);
+    }
+    //delete student
+    public void deleteStudent(String id){
+        StudentModel existingStudent = repository.findById(id).orElseThrow(() -> new RuntimeException("No student found"));
+        repository.deleteById(id);
     }
 }
