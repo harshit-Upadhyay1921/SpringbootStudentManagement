7a559c74d24c24fd9acf43a0328c9be8a5c26514
harshit-Upadhyay1921 <harshitupadhyay1921@gmail.com>
2026-02-10 15:40:45 +0530

    added authorization feature

---
+++ Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.day3studentmanagementsystem.controller;
+
+import com.example.day3studentmanagementsystem.dto.LoginRequestDto;
+import com.example.day3studentmanagementsystem.dto.RegisterRequestDto;
+import com.example.day3studentmanagementsystem.dto.TokenResponseDto;
+import com.example.day3studentmanagementsystem.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService service;
+
+    public AuthController(AuthService service){ this.service = service; }
+
+    @PostMapping("/login")
+    public TokenResponseDto login(@RequestBody LoginRequestDto dto) { return service.login(dto); }
+
+    @PostMapping("/register")
+    public TokenResponseDto register(@Valid @RequestBody RegisterRequestDto dto) { return service.register(dto); }
+}
--- Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
+++ Day3StudentManagementSystem/src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
@@ -1,45 +1,58 @@
 package com.example.day3studentmanagementsystem.controller;
 
+
+
 import com.example.day3studentmanagementsystem.dto.StudentRequestDto;
 import com.example.day3studentmanagementsystem.dto.StudentResponseDto;
 import com.example.day3studentmanagementsystem.model.StudentModel;
 import com.example.day3studentmanagementsystem.service.StudentService;
+import com.example.day3studentmanagementsystem.util.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
     private final StudentService service;
-    public StudentController(StudentService service){
-        this.service = service;
+    private final JwtUtil jwtUtil;
+
+    public StudentController(StudentService service,JwtUtil jwtUtil){
+        this.service=service;
+        this.jwtUtil=jwtUtil;
     }
+
+    private void chechToken(String authHeader){
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            throw new RuntimeException("Invalid token");
+        }
+        String token=authHeader.substring(7);
+        jwtUtil.validateTokenAndGetEmail(token);
+    }
+
+
     @PostMapping("/add-student")
-    public StudentResponseDto addStudent(@Valid @RequestBody StudentRequestDto student){
+    public StudentResponseDto addStudent(
+            @RequestHeader("Authorization") String authHeader,
+            @Valid @RequestBody StudentRequestDto student){
         return service.addStudent(student);
     }
     @GetMapping("/get-student")
-    public List<StudentResponseDto> getStudent(){
+    public List<StudentResponseDto> getStudent(
 
+            @RequestHeader( value="Authorization",required=false) String authHeader
+    ){
+        chechToken(authHeader);
         return service.getStudent();
     }
     @PutMapping("/update/{id}")
-    public StudentResponseDto updateStudent(@PathVariable String id,@RequestBody StudentRequestDto student){
+    public StudentResponseDto updateStudent(@PathVariable String id,@Valid @RequestBody StudentRequestDto student){
         return service.updateStudent(id,student);
     }
-    @PatchMapping("/update-field/{id}")
-    public StudentResponseDto updateStudentField(
-            @PathVariable String id,
-            @RequestBody Map<String, Object> updates) {
-
-        return service.updateStudentField(id, updates);
-    }
-
     @DeleteMapping("/delete-student/{id}")
     public void deleteStudent(@PathVariable String id){
         service.deleteStudent(id);
     }
+
 }
